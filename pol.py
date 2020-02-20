280fd276eab933a53f185a4c228c6c2493dac4ab
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-02-20 20:03:02 -0300

    Add files via upload

--- pol.py
+++ pol.py
@@ -1,19 +1,5 @@
 import tkinter as tk
-'''frase = input('Digite a sua frase: \n')
-espaços = 0
-caractere = 0
-frasesemespaco = ""
 
-    
-  for letra in range(0,len(frase)):
-
-    if frase[letra] == " ":
-        espaços += 1
-    else:
-        frasesemespaco += frase[letra]
-        caractere += 1
-    
-print ("1. - Total de caracteres encontrados:  {}   \n2. - Total de espaços em branco encontrados: {}  \n3. – Boanoiteturmadecompiladores: {}".format(caractere,espaços,frasesemespaco))'''
 root= tk.Tk()
 
 canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
@@ -47,24 +33,24 @@ def Analisador ():
             
     #print(frase)
     
-    label3 = tk.Label(root, text= 'A frase que você digitou é ' + frase,font=('helvetica', 10))
+    label3 = tk.Label(root, text= 'A frase que você digitou é: ' + frase,font=('helvetica', 10))
     canvas1.create_window(200, 210, window=label3)
     
     #label4 = tk.Label(root, text= frase,font=('helvetica', 10, 'bold'))
     #canvas1.create_window(240, 230, window=label4)
     
-    label5 = tk.Label(text= 'Total de caracteres encontrados:' + str(caractere),font=('helvetica', 10))
-    canvas1.create_window(250, 240, window=label5)
+    label5 = tk.Label(text= 'Total de caracteres encontrados: ' + str(caractere),font=('helvetica', 10))
+    canvas1.create_window(200, 240, window=label5)
     
-    label6 = tk.Label(text= 'Total de espaços em branco encontrados:' + str(espaços),font=('helvetica', 10))
-    canvas1.create_window(300, 260, window=label6)
+    label6 = tk.Label(text= 'Total de espaços em branco encontrados: ' + str(espaços),font=('helvetica', 10))
+    canvas1.create_window(200, 260, window=label6)
     
-    label7 = tk.Label(text= 'Frase sem nenhum espaço (concatenado) :' + str(frasesemespaco),font=('helvetica', 10))
-    canvas1.create_window(380, 270, window=label7)
+    label7 = tk.Label(text= 'Frase sem nenhum espaço (concatenado): ' + str(frasesemespaco),font=('helvetica', 10))
+    canvas1.create_window(200, 280, window=label7)
     
     
     
 button1 = tk.Button(text='Analisar', command=Analisador, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
-canvas1.create_window(310, 135.8, window=button1)
+canvas1.create_window(325, 138, window=button1)
 
-root.mainloop()
+root.mainloop() 
